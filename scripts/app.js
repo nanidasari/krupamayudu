243fc49485379267191726b5bd16218007f29691
Isaac Dasari <90327970+nanidasari@users.noreply.github.com>
2023-01-27 19:21:35 +0530

    new
    file

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+function opn(){
+        
+    document.getElementById("mnav").style.marginLeft = "0px";
+    
+    
+  }
+  function cl(){
+    
+    document.getElementById("mnav").style.marginLeft = "-1000px";
+  }
+  
